e90429fa0cb022fefbb4a15e14d91f3aaa246a5f
faizamahmoud <faiza.mahmoud@gmail.com>
2023-02-25 13:55:47 -0700

    contains duplicate: sort and hash solutions

--- functions.js
+++ functions.js
@@ -16,7 +16,41 @@ const bruteForce = (nums) => {
 return false
 }
 
+/*
+Sorting approach: You can sort the array and then check for adjacent elements that are equal. 
+If any two adjacent elements are equal, return true; otherwise, return false. This approach 
+has a time complexity of O(n log n), where n is the length of the array, because sorting takes 
+O(n log n) time.
+*/
+const sortSolution = (nums) => {
+    // O(nLogn)
+    nums.sort();
 
+    // O(n)
+    // start i=1 to compare that and the element previous
+    for (let i = 1; i < nums.length; i++) {
+      if (nums[i] === nums[i - 1]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+
+// a value in set may only occur once(set of unique values)
+// O(n)
+function hashSolution(nums) {
+    const seen = new Set();
+    for (let num of nums) {
+        //verify if the new set contains num
+        if (seen.has(num)) {
+        return true;
+      }
+      seen.add(num);
+    }
+    return false;
+  }
+  
 module.exports = {
     bruteForce: bruteForce,
 };
